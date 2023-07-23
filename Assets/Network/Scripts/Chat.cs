5159c563e0031c350027223823686d3c20624b53
jungtaek kim <kgajungtaek@gmail.com>
2023-07-23 21:37:32 +0900

    feat : Chat 구현

---
+++ Assets/Network/Scripts/Chat.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chat : MonoBehaviour
+{
+    [SerializeField] TMP_InputField inputField;
+    [SerializeField] RectTransform content;
+    [SerializeField] ScrollRect scrollRect;
+
+    [SerializeField] TMP_Text chatTextPrefab;
+
+    private void Awake()
+    {
+        inputField.onSubmit.AddListener(SendChat);
+    }
+
+    public void AddMessage(string message)
+    {
+        TMP_Text newMessage = Instantiate(chatTextPrefab);
+        newMessage.text = message;
+        newMessage.transform.SetParent(content);
+        scrollRect.verticalScrollbar.value = 0;
+    }
+
+    public void SendChat(string chat)
+    {
+        if (inputField.text != "")
+            AddMessage(chat);
+
+        inputField.text = "";
+        inputField.ActivateInputField();
+    }
+}
