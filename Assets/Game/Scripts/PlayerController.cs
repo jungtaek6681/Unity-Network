c83b1dd56098cc6ea3ab2a9f7e703ed57c412126
jungtaek kim <kgajungtaek@gmail.com>
2023-07-26 02:31:41 +0900

    feat : PhotonView 동기화

--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -3,14 +3,48 @@ using Photon.Pun.UtilityScripts;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviourPun
 {
     [SerializeField] List<Color> playerColor;
+    [SerializeField] float movePower;
+    [SerializeField] float rotateSpeed;
+    [SerializeField] float maxSpeed;
+
+    private Rigidbody rigid;
+    private Vector2 inputDir;
 
     private void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
+
         SetPlayerColor();
+    }
+
+    private void Update()
+    {
+        Accelate(inputDir.y);
+        Rotate(inputDir.x);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        inputDir = value.Get<Vector2>();
+    }
+
+    private void Accelate(float input)
+    {
+        rigid.AddForce(input * movePower * transform.forward, ForceMode.Force);
+        if (rigid.velocity.magnitude > maxSpeed)
+        {
+            rigid.velocity = rigid.velocity.normalized * maxSpeed;
+        }
+    }
+
+    private void Rotate(float input)
+    {
+        transform.Rotate(Vector3.up, input * rotateSpeed * Time.deltaTime);
     }
 
     private void SetPlayerColor()
